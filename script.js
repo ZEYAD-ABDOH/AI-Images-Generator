bbfa4c371ddd7e6808ae13addb38fd63258a09da
ZEYAD-ABDOH <86929628+ZEYAD-ABDOH@users.noreply.github.com>
2023-11-29 19:56:29 +0300

    this is frist App with OpenAi
    AI Images Generator

---
+++ script.js
@@ -0,0 +1,35 @@
+const api = "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";
+const inp = document.getElementById('inp');
+const images = document.querySelector('.images')
+const getimage =async ()=>{
+  console.log("zeyad")
+  // make request to openia api
+  const  methods = {
+    method:"POST",
+    headers:{
+      "Content-Type" : "application/json",
+      "Authorization": `Bearer ${api}`
+    },
+    body:JSON.stringify(
+    {
+    "prompt":inp.value,
+    "n": 3, 
+    "size": "256x256"
+    }
+    ) 
+  }
+
+    const res = await fetch("https://api.openai.com/v1/images/generations" ,methods)
+ // parse the  response as json
+    const data = await res.json()
+    const listImages = data.data;
+    images.innerHTML=""
+    listImages.map(photo=> {
+    const container = document.createElement("div")
+    images.append(container)
+    const img  = document.createElement("img")
+    container.append(img)
+    img.src = photo.url
+    
+    })
+}
